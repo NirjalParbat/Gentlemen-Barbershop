9f02b75f8539b48e97438847ac3834ef29b35b11
Nirjal Parbat <nirjalparbat@gmail.com>
2024-07-28 04:34:26 +0545

    Gentlemen Barbershop First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+
+  theme: {
+    extend: {
+      fontFamily:{
+        Amsterdam:["Amsterdam" , "sans-serif"],
+      
+      
+        Holtwood:["Holtwood", "sans-serif"],
+      
+      
+        Crimson:["Crimson", "sans-serif"],
+      
+      
+        Montserrat:["Montserrat", "sans-serif"],
+
+        MontserratRegular:["MontserratRegular", "sans-serif"],
+      
+        Playfair:["Playfair", "sans-serif"],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
